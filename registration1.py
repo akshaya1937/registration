f408f37942013b2f7c74c958241686d93b4bf0f3
akshaya1937 <vadlaakshaya191@gmail.com>
2025-12-18 11:41:02 +0530

    Add files via upload

---
+++ registration1.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import sqlite3
+
+conn=sqlite3.connect("user,db",check_same_thread=False)
+cursor = conn.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS
+              users(name text PRIMARY KEY,password text)""")
+conn.commit()
+choice=st.sidebar.selectbox("MENU",["LOGIN","REGISTER"])
+if choice=="REGISTER":
+    name=st.text_input("USERNAME")
+    password=st.text_input("PASSWORD",type="password")
+    if st.button("REGISTER"):
+        cursor.execute(""" INSERT OR IGNORE INTO users(name,password)VALUES(?,?)""",
+                       (name,password))
+        conn.commit()
+        st.balloons()
+if choice=="LOGIN":
+    name=st.text_input("USERNAME")
+    password=st.text_input("PASSWORD",type="password")
+    if st.button("LOGIN"):
+        cursor.execute("""SELECT * FROM users WHERE
+        name=? AND password=?""",(name,password))
+        result=cursor.fetchone()
+        if result:
+            st.success("valid user")
+            st.snow()
+        else:
+            st.error("invalid user")
